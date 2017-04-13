c64420b0580879b8a42afb9cc9edbe25518786f8
omer <omer.mukhtar8@gmail.com>
2017-04-13 16:57:09 +0500

    added attribues

---
+++ db/migrate/20170413112418_addition_to_users.rb
@@ -0,0 +1,8 @@
+class AdditionToUsers < ActiveRecord::Migration
+  def change
+  add_column :users, :bio, :text
+  add_column :users, :age, :integer
+  add_column :users, :gender, :string
+
+  end
+end

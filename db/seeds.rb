55bdd257ab4bf6c6ed0ecd23c0222dfa6e980fc2
omer <omer.mukhtar8@gmail.com>
2017-04-14 10:34:44 +0500

    test data created using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "Omer",email: "omer@gmail.com", password: "Lovestuk1", password_confirmation: "Lovestuk1" )
+User.create(username: "Mary",email: "oer@gmail.com", password: "Lovestuk2", password_confirmation: "Lovestuk2" )
+User.create(username: "Oer",email: "ome@gmail.com", password: "Lovestuk3", password_confirmation: "Lovestuk3" )
+User.create(username: "Omr",email: "mer@gmail.com", password: "Lovestuk4", password_confirmation: "Lovestuk4" )
+User.create(username: "Or",email: "or@gmail.com", password: "Lovestuk5", password_confirmation: "Lovestuk5" )
+p "tests created"
